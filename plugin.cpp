8fa9b8d264d6bcb254ae79bf26ab0593669dd831
Mark Riddoch <mark@dianomic.com>
2019-07-18 09:52:31 +0000

    Remove duplicate definition of QUOTE macro

--- plugin.cpp
+++ plugin.cpp
@@ -19,10 +19,6 @@
 #include <fft_filter.h>
 #include <version.h>
 
-#define TO_STRING(...) DEFER(TO_STRING_)(__VA_ARGS__)
-#define DEFER(x) x
-#define TO_STRING_(...) #__VA_ARGS__
-#define QUOTE(...) TO_STRING(__VA_ARGS__)
 
 #define FILTER_NAME "fft"
 const char *def_cfg = QUOTE({
